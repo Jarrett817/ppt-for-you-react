550cf3db554809b3dc71584bcae6c9e3efb79722
Jarrett <1206905682@qq.com>
2020-11-10 18:46:24 +0800

    尝试示例

--- src/views/Presentation.jsx
+++ src/views/Presentation.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-function Presentation(){
-    let deck = new Reveal({
-        plugins: [ Markdown ]
-    })
-    deck.initialize();
-    return(
-        <div></div>
+
+function Presentation() {
+    useEffect(() => {
+        Reveal.initialize(
+            {
+                plugins:[
+                    Markdown
+                ]
+            }
+        );
+    }, [])
+
+    return (
+        <div className="reveal">
+            <div className="slides">
+               <section>
+                   <h1>第一页</h1>
+               </section>
+                <section>
+                    <h2>第二页</h2>
+                </section>
+            </div>
+        </div>
     )
 }
+
 export default Presentation
